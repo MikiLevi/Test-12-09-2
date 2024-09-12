4eae28303a4584f919bfbee8964ae588cfde72bb
Miki_Levi <miclevi101@gmail.com>
2024-09-12 16:26:36 +0300

    html and css

---
+++ script.js
@@ -0,0 +1,16 @@
+const full_name = document.querySelector(`#name`)
+const rank = document.querySelector(`#rank`)
+const position = document.querySelector(`#position`)
+const platoon = document.querySelector(`#platoon`)
+const mission = document.querySelector(`#mission`)
+const select = document.querySelector(`#select`)
+const person = document.querySelector(`add-person`)
+const table = document.querySelector(`.table`)
+const tbody = document.querySelector(`#tbody`)
+
+const nwe_tr = document.createElement(`tr`)
+
+function add(){
+    if 
+    tbody.append(nwe_tr)
+}
